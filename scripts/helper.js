64efbc4405496bab0c8b1d591b0118b3bff44fc8
Jhlechia Qureshi <jhlechiae@gmail.com>
2016-04-01 09:07:13 -0400

    Added project info to each individual project

--- scripts/helper.js
+++ scripts/helper.js
@@ -12,22 +12,22 @@ for(var i=0; i < skills_array.length; i += 1) {
 var projects = '<li><h3>%data%</h3><div class="row"><div class="col-md-3"><img src="./img/%data1%.gif" alt="project-images" class="img-responsive"></div><div class="col-md-9"><h3>Language(s): %data2%</h3><h5>%data3%</h5></div></div></li><br><hr>';
 var projects_array = [
   {
-    name: 'Great Escape REPL game',
+    name: 'Great Escape',
     img: 'repl' ,
     language: 'Ruby',
-    info: 'A read–eval–print loop (REPL), also known as an interactive toplevel or language shell, is a simple, interactive computer programming environment that takes single user inputs (i.e. single expressions), evaluates them, and returns the result to the user; a program written in a REPL environment is executed piecewise.'
+    info: 'This was one of my favorite projects, even though on a user level it looks pretty basic. Great Escape is a REPL game that can be played on the command line.  It takes single user inputs, evaluates them, and returns results based on that input. The excitement in creating this project, is that the results are literally endless. You have full creative control over what your game is about and where it will take a player, which is one of the major reasons it was so much fun to make.  Additional features would be to add some graphics to go along with the game.'
   },
   {
     name:'BrandPix',
     img:'brand',
     language: 'Ruby, RubyonRails, HTML, CSS,',
-    info: 'A read–eval–print loop (REPL), also known as an interactive toplevel or language shell, is a simple, interactive computer programming environment that takes single user inputs (i.e. single expressions), evaluates them, and returns the result to the user; a program written in a REPL environment is executed piecewise.'
+    info: 'The idea behind BrandPix stemmed from a need for companies to get user input on product/brand launches prior to spending billions of dollars on failed ideas.  It was my first opportunity to utilize the Ruby on Rails framework and was done so in a group setting.  I think of it as my Rails guinea pig, being that is the first project I created after very recently learning Rails.  Additional features would be improvement to the GUI.'
   },
   {
-    name:'AmpedUp',
+    name:'Amped Up',
     img:'amped',
     language: 'Ruby, RubyonRails, HTML, CSS, Javascript, JQuery, APIs, Twitter Bootstrap',
-    info: 'A read–eval–print loop (REPL), also known as an interactive toplevel or language shell, is a simple, interactive computer programming environment that takes single user inputs (i.e. single expressions), evaluates them, and returns the result to the user; a program written in a REPL environment is executed piecewise.'
+    info: 'Amped Up was born out of a love for music and concerts and recognition of a need for social activities centered around that love. Amped Up allows users going to the same concert to create pre and post concert events, as well as carpools to the specific concert.  It was created using the Rails framework, in addition to Twitter Bootstrap for the GUI. We also used the 5gig and Google Maps APIs.'
   }
 ];
 
